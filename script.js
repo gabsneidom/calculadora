1c1f9ef885981989e11052ec1fd0bb6ab35016a0
gabsneidom <gabsneidom@gmail.com>
2025-09-10 10:26:37 -0300

    Inserção do arquivo script e da função selecionarNumero

---
+++ script.js
@@ -0,0 +1,11 @@
+function selecionarNumero(botao) {
+    const numero = botao.innerText;
+
+    const display = document.getElementById("display");
+
+    display.innerText += numero;
+}
+
+function somar() {
+
+}
